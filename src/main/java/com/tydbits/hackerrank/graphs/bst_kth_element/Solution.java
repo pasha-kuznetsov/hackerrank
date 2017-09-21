358300f1b5a81a6ffa8b8b11502c5bb7858e30a6
Pasha Kuznetsov <pasha@tydbits.com>
2017-09-20 22:14:16 -0500

    BST k-th element progress

--- src/main/java/com/tydbits/hackerrank/graphs/bst_kth_element/Solution.java
+++ src/main/java/com/tydbits/hackerrank/graphs/bst_kth_element/Solution.java
@@ -1,18 +1,103 @@
 package com.tydbits.hackerrank.graphs.bst_kth_element;
 
 import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(deleteKthLargest(System.in));
+        deleteKthLargest(System.in, System.out);
     }
 
-    static String deleteKthLargest(InputStream in) {
-        return "";
+    static void deleteKthLargest(InputStream in, PrintStream out) {
+        Scanner scanner = new Scanner(in);
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+
+        Tree tree = new Tree();
+        for (int i = 0; i < n; ++i)
+            tree.add(scanner.nextInt());
+
+        // TODO: find k-th
+        Tree.Node node = tree.findParent(Integer.MAX_VALUE);
+        tree.delete(node);
+
+        for (Tree.BFS bfs = new Tree.BFS(tree.root); bfs.hasNext(); ) {
+            out.print(bfs.next().key);
+            out.print(' ');
+        }
+    }
+}
+
+class Tree {
+    Node root;
+
+    void add(int key) {
+        if (root == null)
+            root = new Node(key);
+        else
+            add(root, new Node(key));
     }
 
-    class Node {
+    private void add(Node parent, Node node) {
+        findParent(parent, node.key).setChild(node);
+    }
+
+    Node findParent(int key) {
+        return findParent(root, key);
+    }
+
+    private Node findParent(Node parent, int key) {
+        for (Node child = parent; child != null; child = parent.getChild(key))
+            parent = child;
+        return parent;
+    }
+
+    void delete(Node node) {
+        Node parent = root, child = parent;
+        for (; child != node && child != null; child = parent.getChild(node.key))
+            parent = child;
+        if (parent.left == child) parent.left = null;
+        if (parent.right == child) parent.right = null;
+        // TODO: rotate etc
+    }
+
+    static class BFS {
+        Queue<Node> queue = new ArrayDeque<>();
+
+        BFS(Node root) { queue.add(root); }
+
+        boolean hasNext() { return !queue.isEmpty(); }
+
+        Node next() {
+            Node node = queue.poll();
+            if (node == null) return null;
+            if (node.left != null) queue.add(node.left);
+            if (node.right != null) queue.add(node.right);
+            return node;
+        }
+    }
+
+    static class Node {
         int key;
         Node left, right;
+
+        Node(int key) { this.key = key; }
+
+        Node getChild(int key) { return key <= this.key ? left : right; }
+
+        void setChild(Node child) {
+            if (child.key <= this.key)
+                left = child;
+            else
+                right = child;
+        }
+
+        @Override
+        public String toString() {
+            return Integer.toString(key);
+        }
     }
 }
--- src/test/groovy/com/tydbits/hackerrank/graphs/bst_kth_element/BstTest.groovy
+++ src/test/groovy/com/tydbits/hackerrank/graphs/bst_kth_element/BstTest.groovy
@@ -8,20 +8,25 @@ import java.nio.charset.StandardCharsets
 class BstTest extends Specification {
 
     @Unroll
-    def "test"() {
+    def "test #expectedOutput"() {
         given:
         def inputStream = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8.name()))
+        def outputStream = new ByteArrayOutputStream()
+        def printStream = new PrintStream(outputStream, true);
 
-        expect:
-        Solution.deleteKthLargest(inputStream) == expectedOutput
+        when:
+        Solution.deleteKthLargest(inputStream, printStream)
+
+        then:
+        outputStream as String == expectedOutput
 
         where:
         input                        | expectedOutput
         '''
         5 1
-        4 2 7 1 3'''      | '4 2 1 3'
+        4 2 7 1 3'''      | '4 2 1 3 '
         '''
         6 5
-        10 8 12 7 11 9''' | '10 9 12 7 11'
+        10 8 12 7 11 9''' | '10 9 12 7 11 '
     }
 }
